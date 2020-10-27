025685a5c5749a66b646b890c2b1e404e99b2713
osikGit <ostpgrn@gmail.com>
2020-10-27 12:32:51 +0200

    Added joystick movement, rotation by joystick

---
+++ CrewProjectBL/Assets/FolowCharacter.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FolowCharacter : MonoBehaviour
+{
+    private Transform player;
+
+    void Start()
+    {
+        player = FindObjectOfType<Movement>().transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = player.position;
+    }
+}
--- CrewProjectBL/Assets/Scripts/Movement.cs
+++ CrewProjectBL/Assets/Scripts/Movement.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
@@ -7,17 +8,50 @@ using UnityEngine.UI;
 public class Movement : MonoBehaviour
 {
     public float speed = 0.5f;
+    public float rotationSpeed = 0.5f;
+
+    private Transform meshTransform;
+
     public NavMeshAgent agent;
+    private Joystick joystick;
     // Start is called before the first frame update
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
+
+        meshTransform = GetComponentInChildren<Collider>().transform;
+
+        joystick = FindObjectOfType<Joystick>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        CheckMovement(); 
+        CheckMovement();
+        CheckJoystickMovement();
+        CheckRotation();
+    }
+
+    private void CheckRotation()
+    {
+        Vector2 direction = joystick.Direction;
+
+        Vector3 result = new Vector3(joystick.Horizontal, 0, joystick.Vertical);
+
+        if (result != Vector3.zero)
+        {
+            meshTransform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(result), rotationSpeed*Time.deltaTime);
+        }
+    }
+
+    public void CheckJoystickMovement()
+    {
+        float horizontal = joystick.Horizontal;
+        float vertical = joystick.Vertical;
+
+        Vector3 destination = new Vector3(horizontal * speed * Time.deltaTime, 0, vertical * speed * Time.deltaTime);
+
+        agent.Move(destination);
     }
     public void CheckMovement()
     {
@@ -32,5 +66,13 @@ public class Movement : MonoBehaviour
     public void ChangeSpeed(Scrollbar value)
     {
         speed = value.value*30;
+    }
+    public Quaternion GetRotation(Vector2 direction)
+    {
+        Vector3 resultVector = new Vector3();
+        resultVector.x = direction.x;
+        resultVector.z = direction.y;
+        Quaternion quaternion = Quaternion.Euler(resultVector);
+        return quaternion;
     }
 }
